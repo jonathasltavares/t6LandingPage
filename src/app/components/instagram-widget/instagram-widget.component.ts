1e275d365371cdb83ce7420456a843b3dbefa106
Jonathas Tavares <jonathasl.tavares@gmail.com>
2024-01-06 13:20:29 -0300

    criando redes sociais section

--- src/app/components/instagram-widget/instagram-widget.component.ts
+++ src/app/components/instagram-widget/instagram-widget.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-instagram-widget',
   templateUrl: './instagram-widget.component.html',
-  styleUrls: ['./instagram-widget.component.css']
+  styleUrls: ['./instagram-widget.component.css'],
 })
 export class InstagramWidgetComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    (<any>window).instgrm.Embeds.process();
   }
-
 }
--- src/app/components/twitter-widget/twitter-widget.component.ts
+++ src/app/components/twitter-widget/twitter-widget.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-twitter-widget',
   templateUrl: './twitter-widget.component.html',
-  styleUrls: ['./twitter-widget.component.css']
+  styleUrls: ['./twitter-widget.component.css'],
 })
 export class TwitterWidgetComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    (<any>window).twttr.widgets.load();
   }
-
 }
